b57af098b119a0b742f613d3628485a8c0979d87
JokerYan <770792058@qq.com>
2021-09-17 00:06:28 +0800

    init kl loss

--- post_utils.py
+++ post_utils.py
@@ -144,6 +144,7 @@ def post_train(model, images, train_loader, train_loaders_by_class, args):
                                 params=model.parameters(),
                                 momentum=0.9,
                                 nesterov=True)
+    kl_loss = nn.KLDivLoss()
     # target_bce_loss_func = TargetBCELoss()
     # target_bl_loss_func = TargetBLLoss()
     images = images.detach()
@@ -246,7 +247,7 @@ def post_train(model, images, train_loader, train_loaders_by_class, args):
                 raise NotImplementedError
             # adv_class = torch.argmax(adv_output)
             # loss_pos = loss_func(adv_output, label)
-            loss_kl = nn.KLDivLoss(adv_output, original_output)
+            loss_kl = kl_loss(adv_output, original_output)
             # loss_trades = trades_loss(model, data, label, optimizer)
             # loss_neg = loss_func(adv_output, target)
             # bce_loss = target_bce_loss_func(adv_output, label, original_class, neighbour_class)
