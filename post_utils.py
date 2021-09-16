f11a1fc1c55d087a8ade308e007fdab803696b52
JokerYan <770792058@qq.com>
2021-09-17 00:08:32 +0800

    use both original and kl loss

--- post_utils.py
+++ post_utils.py
@@ -247,6 +247,7 @@ def post_train(model, images, train_loader, train_loaders_by_class, args):
                 raise NotImplementedError
             # adv_class = torch.argmax(adv_output)
             # loss_pos = loss_func(adv_output, label)
+            loss_ori = loss_func(original_output, label)
             loss_kl = kl_loss(adv_output, original_output)
             # loss_trades = trades_loss(model, data, label, optimizer)
             # loss_neg = loss_func(adv_output, target)
@@ -254,7 +255,7 @@ def post_train(model, images, train_loader, train_loaders_by_class, args):
             # bl_loss = target_bl_loss_func(adv_output, label, original_class, neighbour_class)
 
             # loss = torch.mean(loss_list)
-            loss = loss_kl
+            loss = loss_ori + loss_kl
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
